66065103145dde17aaf5546e91156a3fa7e7ab4d
fjerbaek <jacob.f.olsen@gmail.com>
2020-04-13 17:26:08 +0200

    added banditten!

---
+++ commands/banditten.js
@@ -0,0 +1,21 @@
+const channelUtils = require('../utils/channelutils.js');
+const soundUtils = require('../utils/soundutils.js');
+const {voiceid, kammerid, formid} = require('../config.json');
+module.exports = {
+    name: 'banditten',
+    usage: '',
+    description: 'Ryk i banditten!',
+    execute(message, args) {
+        if(!(message.channel.id === kammerid)){
+            return channelUtils.reply(message, "du kan altså kun rykke i banditten på #kammeret");
+        } else {
+            pullBandit(message)
+        }
+    }
+};
+
+function pullBandit(message){
+    soundUtils.play(message.client.voiceconnection, "./sound/banditten.mp3");
+    channelUtils.sendMessage(message.channel, message.member.displayName + " rykkede i banditten")
+}
+
