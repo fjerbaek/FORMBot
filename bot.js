5e50af286e406327d2cdb134c82f24c02f7cb326
fjerbaek <jacob.f.olsen@gmail.com>
2020-05-07 20:31:57 +0200

    Minor bug fix

--- bot.js
+++ bot.js
@@ -123,9 +123,9 @@ client.on('voiceStateUpdate', (oldMember, newMember) => {
     if (newMember.channelID === teleid && !newMember.channel.full) {
         soundUtils.play(client.voiceconnection, "./sound/telefon.mp3");
     //If the person responding from kammeret goes back to kammeret.
-    } else if (oldMember.channelID === teleid && newMember.channelID === voiceid) {
+    } else if (oldMember.channelID === teleid && newMember.channelID === voiceid && oldMember.channel.full) {
         soundUtils.play(client.voiceconnection, "./sound/hangup.mp3");
-    } else if (oldMember.channelID === teleid || (newMember.channelID === teleid && newMember.channel.full)) {
+    } else if (oldMember.channelID === teleid|| (newMember.channelID === teleid && newMember.channel.full)) {
         soundUtils.play(client.voiceconnection, "./sound/silence.mp3");
     }
 });
