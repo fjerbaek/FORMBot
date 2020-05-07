c954f7c0f0f5211e17b92a951d1f6ee21a7e760a
fjerbaek <jacob.f.olsen@gmail.com>
2020-05-07 22:09:45 +0200

    P=NP

---
+++ commands/np.js
@@ -0,0 +1,12 @@
+const channelUtils = require('../utils/channelutils.js');
+const soundUtils = require('../utils/soundutils.js');
+const {voiceid, kammerid, formid} = require('../config.json');
+module.exports = {
+    name: 'p',
+    description: 'P = NP',
+    execute(message, args) {
+            channelUtils.reply(message, "Mente du '!p'?")
+        }
+    }
+};
+
